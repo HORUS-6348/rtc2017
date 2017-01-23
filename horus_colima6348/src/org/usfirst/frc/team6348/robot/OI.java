41c2aa5cd654380b70e7c6ca6b04c1a156edff5d
Emilio Mendoza <emilio2601@gmail.com>
2017-01-23 12:12:26 -0600

    Agregué dos objetos joystick

--- horus_colima6348/src/org/usfirst/frc/team6348/robot/OI.java
+++ horus_colima6348/src/org/usfirst/frc/team6348/robot/OI.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team6348.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 import org.usfirst.frc.team6348.robot.commands.ExampleCommand;
@@ -9,6 +10,8 @@ import org.usfirst.frc.team6348.robot.commands.ExampleCommand;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	Joystick stick0 = new Joystick(0);
+	Joystick stick1 = new Joystick(1);
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
