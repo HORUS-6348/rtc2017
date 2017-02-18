72b59e64c0dc9aa8383a3ac4bc548e96665070dc
Emilio Mendoza <emilio2601@gmail.com>
2017-02-17 19:03:23 -0600

    Refactorización de la clase TrenMotriz
    Dejamos de tener una clase con una God-function y repartimos las tareas
    a diferentes funciones: existen funciones drive() con diferentes
    parámetros que se encargan de las diferentes situaciones de manejo: ya
    sea con joystick, cuando se maneja autónomamente, etc.
    
    Aún falta refactorizar todas las demás invocaciones del tren motriz para
    que se apeguen a llamarlo, en lugar de usar los Sparks directo de la
    clase RobotMap.

--- horus_colima6348/src/org/usfirst/frc/team6348/robot/subsystems/TrenMotriz.java
+++ horus_colima6348/src/org/usfirst/frc/team6348/robot/subsystems/TrenMotriz.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team6348.robot.subsystems;
 
 import org.usfirst.frc.team6348.robot.Robot;
 import org.usfirst.frc.team6348.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,11 +13,6 @@ public class TrenMotriz extends Subsystem {
 
 	Spark motor_izq = RobotMap.motor_izq;
 	Spark motor_der = RobotMap.motor_der;
-	
-	public void parar(){
-		motor_izq.set(0);
-		motor_der.set(0);
-	}
 	
 	/*
 	 * min ->  1
@@ -43,87 +40,18 @@ public class TrenMotriz extends Subsystem {
 	
 	public double getMotorDer(double degrees, double gatillo){
 		if(degrees <= 90){
-			return smoothBetween(90, 0, degrees) * gatillo;
+			return -smoothBetween(90, 0, degrees) * gatillo;
 		} else if(degrees <= 180){
-			return 1 * gatillo;
-		} else if(degrees <= 270){
-			return smoothBetween(180, 270, degrees) * gatillo;
-		} else if(degrees <= 360){
 			return -1 * gatillo;
+		} else if(degrees <= 270){
+			return -smoothBetween(180, 270, degrees) * gatillo;
+		} else if(degrees <= 360){
+			return 1 * gatillo;
 		} else {
 			return 0;
 		}
 	}
 	
-	private double toDegrees(double angrad){
-		double degrees = Math.toDegrees(angrad);
-		if(degrees < 0){
-			return -degrees;
-		} else {
-			return 360 - degrees;
-		}
-	}
-	
-	public void initDefaultCommand(){
-		double gatillo = Robot.oi.stick0.getRawAxis(3);
-		double otroGatillo = Robot.oi.stick0.getRawAxis(2);
-		double gyroAngle = Robot.oi.gyro.getAngle();
-		
-		if(otroGatillo > 0.05){
-			gatillo = (otroGatillo * 0.5) + (gatillo * 0.5);
-		}
-		
-		boolean stateA = Robot.oi.A.get();
-		boolean stateX = Robot.oi.X.get();
-		boolean stateY = Robot.oi.Y.get();
-
-		if(stateA){
-			gatillo = 0.25;
-		} else if(stateX){
-			gatillo = 0.5;
-		} else if(stateY){
-			gatillo = 0.75;
-		}
-		
-		double dPad = Robot.oi.stick0.getPOV(0);
-		
-		if(dPad == -1){ //Si no está presionado el D-PAD, usamos joystick
-			double x = Robot.oi.stick0.getRawAxis(0);
-			double y = Robot.oi.stick0.getRawAxis(1);
-			
-			
-			double deadZone = 0.15;
-			
-			
-			//Los joysticks tienen una zona muerta en su centro, para evitar esto
-			//si ambos valores están muy cercanos al mismo (0.15) los dejamos en cero.
-			if(Math.abs(x) < deadZone && Math.abs(y) < deadZone){
-				x       = 0;
-				y       = 0;
-				gatillo = 0;
-			}
-			
-			double radians = Math.atan2(y,x); //Esta función nos da la dirección del vector en radianes
-			double degrees = toDegrees(radians); //Lo pasamos a grados y lo corregimos a nuestra orientación
-			
-			double potencia_izq = getMotorIzq(degrees, gatillo);
-			double potencia_der = getMotorDer(degrees, gatillo);
-			
-			motor_izq.set(potencia_izq);
-			motor_der.set(-0.95 * potencia_der);
-			System.out.println("INPUTS" + " gatillo: " + gatillo + " gyro degrees: " + gyroAngle + " joystick degrees: " + degrees + "  OUTPUTS izq: " + potencia_izq + " der: " + potencia_der + " izq_full: " + getMotorIzq(degrees, 1) + " der_full: " + getMotorDer(degrees, 1));
-			
-		} else { //Está presionado el D-PAD, por lo que toma prioridad sobre el joystick
-			double potencia_izq = getMotorIzqDpad(dPad, gatillo);
-			double potencia_der = getMotorDerDpad(dPad, gatillo);
-			
-			motor_izq.set(potencia_izq);
-			motor_der.set(0.95 * potencia_der);
-			System.out.println("INPUTS gatillo: " + gatillo + " gyro degrees: " + gyroAngle + " dPad: " + dPad + "   OUTPUTS izq: " + potencia_izq + " der: " + potencia_der + " izq_full: " + getMotorIzqDpad(dPad, 1) + " der_full: " + getMotorDerDpad(dPad, 1));
-			
-		}	
-	}
-
 	private double getMotorIzqDpad(double pad, double gatillo){
 		if(pad == 0){
 			return 1 * gatillo;
@@ -166,6 +94,103 @@ public class TrenMotriz extends Subsystem {
 			return 0;
 		}
 	}
+	
+	private double toDegrees(double angrad){
+		double degrees = Math.toDegrees(angrad);
+		if(degrees < 0){
+			return -degrees;
+		} else {
+			return 360 - degrees;
+		}
+	}
+	
+	public void stop(){
+		drive(0, 0);
+	}
+	
+	public void drive(Joystick stick, boolean dpad){
+		if(dpad && stick.getPOV() != -1){
+			driveDpad(stick);
+		} else {
+			driveStick(stick);
+		}
+		
+	}
+		
+	public void drive(double heading, double gatillo){
+		double potencia_izq = getMotorIzq(heading, gatillo);
+		double potencia_der = getMotorDer(heading, gatillo);
+		
+		motor_izq.set(potencia_izq);
+		motor_der.set(potencia_der);
+		
+	}
+	
+	private void driveDpad(Joystick stick){
+		double gatillo = getGatillo(stick);
+		int    dPad    = stick.getPOV();
+		 
+		double potencia_izq = getMotorIzqDpad(dPad, gatillo);
+		double potencia_der = getMotorDerDpad(dPad, gatillo);
+		
+		motor_izq.set(potencia_izq);
+		motor_der.set(potencia_der);
+		
+	}
+	
+	private void driveStick(Joystick stick){
+		double gatillo = getGatillo(stick);
+		double x = Robot.oi.stick0.getRawAxis(0);
+		double y = Robot.oi.stick0.getRawAxis(1);
+		
+		
+		double deadZone = 0.15;
+		
+		
+		//Los joysticks tienen una zona muerta en su centro, para evitar esto
+		//si ambos valores están muy cercanos al mismo (0.15) los dejamos en cero.
+		if(Math.abs(x) < deadZone && Math.abs(y) < deadZone){
+			x       = 0;
+			y       = 0;
+			gatillo = 0;
+		}
+		
+		double radians = Math.atan2(y,x); //Esta función nos da la dirección del vector en radianes
+		double degrees = toDegrees(radians); //Lo pasamos a grados y lo corregimos a nuestra orientación
+		
+		double potencia_izq = getMotorIzq(degrees, gatillo);
+		double potencia_der = getMotorDer(degrees, gatillo);
+		
+		motor_izq.set(potencia_izq);
+		motor_der.set(potencia_der);
+	}
+
+	private double getGatillo(Joystick stick){
+		double gatillo = stick.getRawAxis(3);
+		double otroGatillo = stick.getRawAxis(2);
+		
+		gatillo = (gatillo * 0.5) + (otroGatillo * 0.5); 
+		
+		boolean stateA = stick.getRawButton(1);
+		boolean stateX = stick.getRawButton(3);
+		boolean stateY = stick.getRawButton(4);
+
+		if(stateA){
+			gatillo = 0.25;
+		} else if(stateX){
+			gatillo = 0.5;
+		} else if(stateY){
+			gatillo = 0.75;
+		}
+		
+		return gatillo;
+		
+	}
+	
+	public void initDefaultCommand(){
+		drive(Robot.oi.stick0, true);
+	}
+
 
 
 }
