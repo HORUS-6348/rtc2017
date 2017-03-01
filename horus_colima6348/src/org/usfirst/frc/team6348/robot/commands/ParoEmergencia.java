c37126eb80697e7f6766883fb6f1965479f153f3
Emilio Mendoza <emilio2601@gmail.com>
2017-03-01 11:25:20 -0600

    Arreglar mensaje en ParoEmergencia

--- horus_colima6348/src/org/usfirst/frc/team6348/robot/commands/ParoEmergencia.java
+++ horus_colima6348/src/org/usfirst/frc/team6348/robot/commands/ParoEmergencia.java
@@ -10,35 +10,28 @@ import org.usfirst.frc.team6348.robot.Robot;
  */
 public class ParoEmergencia extends Command {
 	public ParoEmergencia() {
-		// Use requires() here to declare subsystem dependencies
 		requires(Robot.trenMotriz);
 	}
 
-	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		SmartDashboard.putString("Comando: ", "AutonomoLateral");
+		SmartDashboard.putString("Comando: ", "ParoEmergencia");
 	}
 
-	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
 		Robot.trenMotriz.stop();
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
 		return true;
 	}
 
-	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 	}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
 		Robot.trenMotriz.stop();
