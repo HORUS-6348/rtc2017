93f30bd1a8ecf7b697b999477431d80a2f068c01
Emilio Mendoza <emilio2601@gmail.com>
2017-04-06 17:47:07 -0500

    Eliminar comentarios innecesarios

--- horus_colima6348/src/org/usfirst/frc/team6348/robot/Robot.java
+++ horus_colima6348/src/org/usfirst/frc/team6348/robot/Robot.java
@@ -26,20 +26,15 @@ import org.usfirst.frc.team6348.robot.subsystems.TrenMotriz;
  * directory.
  */
 public class Robot extends IterativeRobot {
-
 	public static TrenMotriz trenMotriz;
 	public static Escalador escalador;
 	public static OI oi;
-
 
 	Command autonomousCommand;
 	UsbCamera camera;
 	SendableChooser<Command> choose;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+
 	@Override
 	public void robotInit() {
 		RobotMap.init();
@@ -71,11 +66,6 @@ public class Robot extends IterativeRobot {
 		
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	@Override
 	public void disabledInit() {
 		trenMotriz.stop();
@@ -87,27 +77,12 @@ public class Robot extends IterativeRobot {
 		
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
 	@Override
 	public void autonomousInit() {
 		autonomousCommand = (Command) choose.getSelected();
 		autonomousCommand.start();
 	}
 	
-
-	/**
-	 * This function is called periodically during autonomous
-	 */
 	@Override
 	public void autonomousPeriodic() {	
 		Scheduler.getInstance().run();
@@ -116,10 +91,6 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
 		if (autonomousCommand != null){
 			autonomousCommand.cancel();
 		}
@@ -128,21 +99,12 @@ public class Robot extends IterativeRobot {
 		trenMotriz.initDefaultCommand();
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
+
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 	}
-	
-	public void robotPeriodic(){
-		
-	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
 	@Override
 	public void testPeriodic() {
 
